fdc9722bfe6af191f5a2e92aba4ce90fb54678c7
grafter-ca <caleb.designer1@gmail.com>
2025-07-12 20:16:22 +0200

    Update table rendering

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -10,6 +10,10 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
 const teacher3: Teacher = {
   firstName: 'John',
   fullTimeEmployee: false,
@@ -17,7 +21,6 @@ const teacher3: Teacher = {
   location: 'London',
   contract: false,
 };
-
 
 const director1: Directors = {
   firstName: 'John',
@@ -28,6 +31,11 @@ const director1: Directors = {
   contract: false
 };
 
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+}
+
 console.log(teacher3);
 console.log(director1);
+console.log(printTeacher('John', 'Doe'));
 
